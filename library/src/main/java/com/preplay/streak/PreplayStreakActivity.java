5bca52ef642218595aea97c24b6a320a03dcf684
Stan <stan.kocken@gmail.com>
2014-10-03 11:52:27 +0200

    Added cookie and javascript compatibility

--- library/src/main/java/com/preplay/streak/PreplayStreakActivity.java
+++ library/src/main/java/com/preplay/streak/PreplayStreakActivity.java
@@ -2,6 +2,9 @@ package com.preplay.streak;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -20,27 +23,21 @@ public class PreplayStreakActivity extends Activity {
         super.onCreate(savedInstanceState);
         mWebView = new WebView(this);
         setContentView(mWebView);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         if (savedInstanceState != null) {
             mWebView.restoreState(savedInstanceState);
         } else {
-            String appId = sAppId;
-            if (appId == null || appId.trim().length() == 0) {
-                int resId = getResources()
-                        .getIdentifier("preplay_streak_app_id", "string", getPackageName());
-                if (resId != 0) {
-                    appId = getString(resId);
-                }
-            }
-            if (appId == null || appId.trim().length() == 0) {
-                appId = "landing";
-            }
-            String url = "http://" + appId + ".streakit.preplaysports.com";
-            mWebView.loadUrl(url);
+            mWebView.loadUrl(getUrlLandingPage());
             mWebView.setWebViewClient(new WebViewClient() {
                 @Override
                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
                     view.loadUrl(url);
                     return true;
+                }
+
+                public void onPageFinished(WebView view, String url) {
+                    CookieSyncManager.getInstance().sync();
                 }
             });
         }
@@ -50,5 +47,21 @@ public class PreplayStreakActivity extends Activity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mWebView.saveState(outState);
+    }
+
+    private String getUrlLandingPage() {
+        String appId = sAppId;
+        if (appId == null || appId.trim().length() == 0) {
+            int resId = getResources()
+                    .getIdentifier("preplay_streak_app_id", "string", getPackageName());
+            if (resId != 0) {
+                appId = getString(resId);
+            }
+        }
+        if (appId == null || appId.trim().length() == 0) {
+            return "http://preplay-share.s3.amazonaws.com/Streakgame/index.html";
+        } else {
+            return "http://" + appId + ".streakit.preplaysports.com";
+        }
     }
 }
